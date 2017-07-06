6bd9a1875d207c59ddb0fd7eb9d5e8028de88e68
Andrew <expandrew@gmail.com>
2017-07-05 22:04:21 -0700

    commit our practice lessons from monday.

---
+++ my-first-typescript/app/typescript-addition.js
@@ -0,0 +1,7 @@
+var findSum = function (firstValue, secondValue) {
+    var sum = firstNumber + secondNumber;
+    alert("The sum of your two numbers is: " + sum);
+};
+var firstNumber = prompt('Please enter a number.');
+var secondNumber = prompt('Enter another number.');
+findSum(firstNumber, secondNumber);
